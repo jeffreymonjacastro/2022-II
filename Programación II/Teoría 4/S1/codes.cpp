65bbfec84f1d412264992f3a71de766b8780a382
Jeffrey Monja Castro <104637993+jeffreymonjacastro@users.noreply.github.com>
2022-08-15 16:19:42 -0500

    Teo - S1

--- Programación II/Teoría 4/S1/codes.cpp
+++ Programación II/Teoría 4/S1/codes.cpp
@@ -1,8 +1,18 @@
 // Semana 1 - Progra II
 
 #include<iostream>
+using namespace std;
 
 int main(){
+    //variables
+    char letra = "a";
+    bool si = true;
+    int entero = 9;
+    float decimal = 3.14;
+    double decimal2 = 3.14159;
+    string cadena = "Hello World";
 
-  return 0;
+    cout<<"Hola mundo"<<letra<<endl;
+    
+    return 0;
 }
