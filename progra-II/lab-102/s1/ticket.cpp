854b63d422f64a4a3dde05ee76210d979a414cc7
Jeffrey Monja Castro <104637993+jeffreymonjacastro@users.noreply.github.com>
2022-08-16 09:18:40 -0500

    Ticket cpp

---
+++ progra-II/lab-102/s1/ticket.cpp
@@ -0,0 +1,22 @@
+// Creando un ticket
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "##################################"<<endl;
+    cout << "United Airlines FLight 7611"<<endl;
+    cout << "On-time-departs in 3 hours 8 mins"<<endl;
+    cout << "JFK---------------> PEK"<<endl;
+    cout << "Departs New York, today"<<endl;
+    cout << "----------------------------------"<<endl;
+    cout << "Time                 Terminal Gate"<<endl;
+    cout << "3:50 PM              1        -"<<endl;
+    cout << "Arrives Beijing, today"<<endl;
+    cout << "Scheduled  6:20 PM   Terminal Gate"<<endl;
+    cout << "5:53  PM             3        -"<<endl;
+    cout << "##################################"<<endl;
+
+    return 0;
+}
