ca05ed2b04500976a6b9dcac844752427bf0476b
Jeffrey Monja Castro <104637993+jeffreymonjacastro@users.noreply.github.com>
2022-08-22 09:05:30 -0500

    Others

--- progra-II/lab-102/s2/ejercicio2.cpp
+++ progra-II/lab-102/s2/ejercicio2.cpp
@@ -10,7 +10,7 @@ int main(){
     cout<<"Ingrese el consumo en kW: ";
     cin>>kw;
 
-    consumo = (kw <= 100) * (kw*0.4522) + (kw > 100)*(100*0.4522 + (kw-100)*0.7);
+    consumo = (kw<=100) * (kw*0.4522) + (kw > 100)*(100*0.4522 + (kw-100)*0.7); //Booleanos
 
     cout<<"El consumo total es: "<<consumo;
 
