7d4275f9e25b3f327b309de855cfcfa7c5bfc4da
Jeffrey Monja Castro <104637993+jeffreymonjacastro@users.noreply.github.com>
2022-09-01 11:30:33 -0500

    Tarea individual Teo

---
+++ progra-II/teo-1/s3/tarea_individual.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+
+/*
+Escriba un programa en C++ que permita ingresar los datos de un estudiante: nombre y apellido, código (de 8 dígitos), usuario  (será el apellido del estudiante) y clave (una combinación de letras minúsculas, mayusculas y dígitos), a través de un menú.
+
+El programa debe verificar si el código tiene 8 dígitos, y si la clave tiene las condiciones señaladas. De lo contrario se pedirá ingresar el dato (pero solo ese dato) de nuevo. Solo se tienen 3 opciones para ingresar los datos correctamente. Luego de ingresar los datos (input), el programa preguntará si se quiere modificar algún dato.
+
+Se abrirá entonces el menú de nuevo para que el usuario escoja un dato para modificar, o ningún dato para salir.
+
+Se debe, finalmente imprimir los datos del estudiante actualizados.
+
+Notas:
+
+El código debe ser eficiente. Debe tener un numero mínimo de instrucciones (líneas de código)
+Las entradas de codigo (input) deben estar claramente especificadas
+El estudiante decide el formato de salidas de código (output)
+Considere la rúbrica del ejercicio (en Canvas)
+
+
+*/
+
+void result(){
+    string nombre,  apellido, codigo, usuario, clave;
+
+}
+
+int main(){
+    result();
+
+    return 0;
+}
