b962cf60fce2519095b00853cfb2ab9aed988cfa
Jeffrey Monja Castro <104637993+jeffreymonjacastro@users.noreply.github.com>
2022-09-13 18:57:39 -0500

    A

---
+++ progra-II/others/Figuras.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+
+void triangulo1(int n){
+    for(int i = 0; i < n; i++)
+        for(int j = 0; j < n; j++)
+        cout<<"*"
+}
+
+
+int main(){
+
+    return 0;
+}
