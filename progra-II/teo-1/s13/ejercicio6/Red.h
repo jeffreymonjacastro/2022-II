cc0411aa02e1e82c80e7095f135d5e3a178dd972
Jeffrey Monja Castro <104637993+jeffreymonjacastro@users.noreply.github.com>
2022-11-10 11:10:45 -0500

    Tarea Fiestas

---
+++ progra-II/teo-1/s13/ejercicio6/Red.h
@@ -0,0 +1,12 @@
+#ifndef P2_CPP_RED_H
+#define P2_CPP_RED_H
+
+#include <iostream>
+
+using namespace std;
+
+class Red {
+
+};
+
+#endif //P2_CPP_RED_H
---
+++ progra-II/teo-1/s13/ejercicio6/procesadores.cpp
@@ -0,0 +1,5 @@
+#include "Red.h"
+
+int main(){
+
+}
